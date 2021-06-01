1af2b36358d49f3e83e56ba59a9b1cfdf69fd309
Amith <amithpasham@gmail.com>
2021-06-02 00:17:26 +0530

    last

--- src/components/App.js
+++ src/components/App.js
@@ -1,73 +1,51 @@
-import React,{useState} from 'react'
+import React, {Component, useState} from "react";
+import '../styles/App.css';
 
-export default function App() {
-  const [key1, setKey1] = useState("")
-  const [key2, setKey2] = useState("")
-  const [state, setstate] = useState("")
-  function fun1(e){
-setKey1(e.target.value)
-  }
-  function fun2(e){
-    setKey2(e.target.value)
-  }
-  function fun(){
-    document.getElementsByTagName("input")[0].value=""
-    document.getElementsByTagName("input")[1].value=""
-    setKey1("")
-    setKey2("")
-    setstate("")
-  }
-  var len=0;var res="";
-  var str1=key1.split("");
-  var str2=key2.split("");
-  function main(){
- 
-  for(let j=0;j<key2.length;j++){
-    var a = str1.indexOf(key2.charAt(j));
-   delete str1[a]}
- 
- for(let j=0;j<key1.length;j++){
-  var a = str2.indexOf(key1.charAt(j));
- delete str2[a]}
-
-for(let i=0;i<key1.length;i++){
-  if(str1[i]!=undefined)
-  len++;
-}
-for(let i=0;i<key2.length;i++){
-  if(str2[i]!=undefined)
-  len++;
-}
- 
- console.log(len)
- len=len%6
+const App =()=> {
+  var arr  = ["Siblings","Friends","Love","Affection","Marriage","Enemy","Please Enter valid input" ];
+  const [getrel ,setRel ] = useState();
   
-  switch(len){
-    case 1:res="Friends"
-    break;
-    case 2:res="Love"
-    break;
-    case 3:res="Affection"
-    break;
-    case 4:res="Marriage"
-    break;
-    case 5:res="Enemy"
-    break;
-    case 0:res="Sibilings"
-    break;
-    default:res="Please Enter valid input"
-    break
+  const calRelation = () => {
+    let input1 = document.querySelector('#inputfirst').value;
+  let input2 = document.querySelector('#inputlast').value;
+    if (input1 === "" || input2 === "") {
+      setRel(6);
+    } else {    
+     input1 = input1.toLowerCase();
+     input2 = input2.toLowerCase();
+      function match(input1, input2) {
+        let count = 0;
+        for (let i in input1){
+          if(input2.includes(input1[i])){
+          ++count;
+          }
+        }
+        return count;
+      }
+      const comm = match(input1,input2);
+      let l1 = Number(input1.length);
+      let l2 = Number(input2.length);
+      let len = Number(comm);
+       let n  = l1+l2-len*2;
+       setRel(n%6);
+    }
   }
-  setstate(res)
-}   
+  const clearRelation = () => {
+     document.querySelector('#inputfirst').value="";
+     document.querySelector('#inputlast').value="";
+     setRel("");
+
+  }
   return (
     <div id="main">
-      <input data-testid="input1" placeholder="input1" onChange={fun1}></input>
-      <input data-testid="input2" placeholder="input2" onChange={fun2}></input>
-      <button data-testid="calculate_relationship" onClick={main}>Calculate Relationship Future</button>
-      <button data-testid="clear" onClick={fun}>Clear</button>
-      {/* <h3 data-testid="answer">{state}</h3>  */}
-      {state?<h3 data-testid="answer">{state}</h3> :""}
-      </div>
+      <label>First Name</label><input type="text" data-testid="input1" id="inputfirst"></input> <br></br>
+      <label>Last Name</label><input type="text" data-testid="input2" id="inputlast"></input><br></br>
+      <button data-testid="calculate_relationship" onClick={calRelation} >Calculate Relationship Future</button>
+      <button data-testid="clear" onClick={clearRelation}>Clear inputs and relationship status</button>
+      <h3 data-testid="answer" id="relation">{arr[getrel]}</h3>
+    </div>
   )
 }
+
+
+export default App;
